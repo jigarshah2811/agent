8b58a699b4b3111f39c5816e9fa4aea1d3826d87
Jigar Shah <jigarshah.ce@gmail.com>
2017-05-13 14:28:41 -0700

    ignore commit

--- websocket-client.py
+++ websocket-client.py
@@ -7,10 +7,10 @@ from flask import Flask, jsonify, abort, make_response, request, url_for
 import time
 import thread
 from decorators import decoratorWithoutArguments, validate_json
-from configCam import configCam
+from configNetwork import configNetwork
 
 fun_dict = {
-	'configCamCmd':configCam
+	'configNetworkCmd':configNetwork
 }
 
 def print_func(json_data):
@@ -21,17 +21,18 @@ def not_found(cmd):
 
 # @auth.login_required
 # @validate_json('username')
-@decoratorWithoutArguments
+# @decoratorWithoutArguments
 def on_message(ws, request):
 	print request
-	request = json.loads(request)['msg']
+	request = json.loads(request)
 	cmd = request['msg']
 	if cmd not in fun_dict:
 		not_found(cmd)
 
 	# REST API, Call relevent API for this command
 	print_func(request)
-	fun_dict[cmd](request)
+	response = fun_dict[cmd](request)
+	ws.send(response)
 
 def on_error(ws, error):
 	print error
@@ -40,12 +41,12 @@ def on_close(ws):
 	print "### closed ###"
 
 def on_open(ws):
+	print "on_open"
 	def run(*args):
-		for i in range(3):
+		for i in range(1):
+			ws.send(u"Hello {0}".format(i))
 			time.sleep(1)
-			ws.send("Hello %d" % i)
-			time.sleep(1)
-			ws.close()
+			# ws.close()
 			print "thread terminating..."
 	thread.start_new_thread(run, ())
 
@@ -59,15 +60,27 @@ def start_client():
     ssl_context.load_cert_chain(certfile, keyfile)
     ws = create_connection("ws://34.210.26.95:9001/", sslopt={"cert_reqs": ssl.CERT_NONE, "check_hostname": False})
     """
-	server_url = "ws://34.210.26.95:9001/"
+	# TODO: Change to secure connection with AWS
+	# server_url = u"wss://127.0.0.1:8000/"
+	server_url = u"wss://54.190.43.131:8000/"
+
+	ssl_opt = {
+		'check_hostname' : False,
+		'ca_certs' : 'server.crt'
+	}
+
+	# conn = websocket.create_connection(server_url, sslopt=ssl_opt)
+
 	websocket.enableTrace(True)
 	ws = websocket.WebSocketApp(server_url,
-					  on_message=on_message,
-					  on_open=on_open,
-					  on_close=on_close,
-					  on_error=on_error,
-					  keep_running=True)
-	ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+								on_message=on_message,
+								on_open=on_open,
+								on_close=on_close,
+								on_error=on_error,
+								keep_running=True)
+	print "Connecting..."
+	ws.run_forever(sslopt=ssl_opt)
+
 
 if __name__ == "__main__":
 	start_client()
